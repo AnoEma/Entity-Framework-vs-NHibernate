a93959c617e4c0e7632ae4351a0c57720db461d2
ano.emangi <ano.emangi@minutoseguros.com.br>
2020-10-02 18:37:40 -0300

    Implementação de Thread

--- CadastroPessoas/FrmPessoa.cs
+++ CadastroPessoas/FrmPessoa.cs
@@ -2,12 +2,15 @@
 using CadastroPessoas.Repositorio;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace CadastroPessoas
 {
     public partial class FrmPessoa : Form
     {
+        private List<Pessoa> _pessoas = new List<Pessoa>();
+        private static readonly object locker = new Object();
         public FrmPessoa()
         {
             InitializeComponent();
@@ -15,15 +18,70 @@ namespace CadastroPessoas
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            PreencherDataGridView();
+            Thread thread = new Thread(PreencherDataGridView);
+            thread.Start();
         }
 
         private void PreencherDataGridView()
         {
+            try
+            {
+                Thread.Sleep(5000);
+                Thread thread = new Thread(PreencherListaPessoas);
+                Thread thread2 = new Thread(PreencherListaPessoas2);
+                thread.Start();
+                thread2.Start();
+                thread.Join();
+                thread2.Join();
+                dgvPessoa.Invoke((MethodInvoker)delegate { dgvPessoa.DataSource = _pessoas; dgvPessoa.Refresh(); });
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void PreencherListaPessoas()
+        {
             IRepositorio<Pessoa> repositorioPessoas = new PessoaRepositorio();
             List<Pessoa> pessoas = repositorioPessoas.SelecionarTodos();
-            dgvPessoa.DataSource = pessoas;
-            dgvPessoa.Refresh();
+            lock (locker)
+            {
+                foreach (Pessoa p in pessoas)
+                {
+                    p.Nome += "Thread 1";
+                    _pessoas.Add(p);
+                    Thread.Sleep(300);
+                }
+            }
+        }
+
+        private void PreencherListaPessoas2()
+        {
+            try
+            {
+                IRepositorio<Pessoa> repositorioPessoas = new PessoaRepositorio();
+                List<Pessoa> pessoas = repositorioPessoas.SelecionarTodos();
+                lock (locker)
+                {
+                    foreach (Pessoa p in pessoas)
+                    {
+                        p.Nome += "Thread 2";
+                        _pessoas.Add(p);
+                        Thread.Sleep(100);
+                    }
+                }
+                throw new Exception("Acabou é Tetra!");
+            }
+            catch(Exception ex)
+            {
+                ExibirErro(ex);
+            }
+        }
+
+        private void ExibirErro(Exception ex)
+        {
+            MessageBox.Show(ex.Message);
         }
 
         private void bntAdicionarPessoa_Click(object sender, EventArgs e)
